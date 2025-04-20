255802e16bbc34ae9bd630055305eaa2e68e7aad
Gilberto Martini <gilberto.madeira@outlook.com>
2025-04-20 15:08:30 -0300

    added a first rest controller

---
+++ example/example/src/main/java/br/com/crossmade/example/FirstController.java
@@ -0,0 +1,19 @@
+package br.com.crossmade.example;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from my first controller";
+    }
+
+    @GetMapping("/hello-2")
+    public String sayHelloAgain() {
+        return "Second Greeting from my first controller";
+    }
+
+}
