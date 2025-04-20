b0199337d890277e33b96167e3b8c4dfbbf18d95
Gilberto Martini <gilberto.madeira@outlook.com>
2025-04-20 14:54:10 -0300

    more cleanup

--- example/example/src/main/java/br/com/crossmade/example/JavaApplication.java
+++ example/example/src/main/java/br/com/crossmade/example/JavaApplication.java
@@ -1,7 +1,5 @@
 package br.com.crossmade.example;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
