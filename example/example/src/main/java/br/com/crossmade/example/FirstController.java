4406475567eb16d2e47d5a66808101be1ad50bc0
Gilberto Martini <gilberto.madeira@outlook.com>
2025-04-20 19:54:50 -0300

    cleanup 2

--- example/example/src/main/java/br/com/crossmade/example/FirstController.java
+++ example/example/src/main/java/br/com/crossmade/example/FirstController.java
@@ -2,43 +2,19 @@ package br.com.crossmade.example;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class FirstController {
 
-    // @GetMapping("/hello")
-    // public String sayHello() {
-    // return "Hello from my first controller";
-    // }
-
-    @GetMapping("/hello/{user-name}")
-    public String pathVar(
-            @PathVariable("user-name") String userName,
-            @RequestParam("q") String q) {
-        return "Hello " + userName + " querystring param: " + q;
-    }
-
-    @GetMapping("/hello-2")
-    public String sayHelloAgain() {
-        return "Second Greeting from my first controller";
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from my first controller";
     }
 
     @PostMapping("/post")
     public String createMessage(@RequestBody String message) {
         return "message received: " + message;
-    }
-
-    @PostMapping("/post-order")
-    public String createOrder(@RequestBody Order order) {
-        return "Order received: " + order;
-    }
-
-    @PostMapping("/post-order-record")
-    public String createOrderRecord(@RequestBody OrderRecord orderRecord) {
-        return "Order Record received: " + orderRecord.toString();
     }
 }
--- example/example/src/main/java/br/com/crossmade/example/OrderRecord.java
+++
@@ -1,5 +0,0 @@
-package br.com.crossmade.example;
-
-public record OrderRecord(String customerName, String productName, int quantity) {
-
-}
