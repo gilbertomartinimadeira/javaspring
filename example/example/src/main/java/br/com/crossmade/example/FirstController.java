d573bbc906f55addc1b2eceec1e3ccc3bbf0520c
Gilberto Martini <gilberto.madeira@outlook.com>
2025-04-21 15:50:03 -0300

    implements a get method to find student by id

--- example/example/src/main/java/br/com/crossmade/example/FirstController.java
+++ example/example/src/main/java/br/com/crossmade/example/FirstController.java
@@ -1,9 +1,14 @@
 package br.com.crossmade.example;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 
@@ -24,4 +29,10 @@ public class FirstController {
         return _repository.save(student);
 
     }
+
+    @GetMapping("/students/{id}")
+    public Optional<Student> findStudentById(@PathVariable Integer id) {
+        return _repository.findById(id);
+    }
+
 }
