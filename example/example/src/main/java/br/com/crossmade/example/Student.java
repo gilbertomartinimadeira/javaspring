26698379f8dc4e5fd52ed834c423103f27ee3acc
Gilberto Martini <gilberto.madeira@outlook.com>
2025-04-21 15:10:33 -0300

    added constraints to table

--- example/example/src/main/java/br/com/crossmade/example/Student.java
+++ example/example/src/main/java/br/com/crossmade/example/Student.java
@@ -1,18 +1,25 @@
 package br.com.crossmade.example;
 
+import org.hibernate.annotations.Collate;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table
+@Table(name = "students")
 public class Student {
 
     @Id
     private Integer id;
+    @Column(name = "first_name", length = 20)
     private String firstname;
+    @Column(name = "last_name", length = 30)
     private String lastname;
+    @Column(name = "email", unique = true)
     private String email;
+    @Column(name = "age", updatable = false)
     private int age;
 
     public Student(String firstname, String lastname, String email, int age) {
