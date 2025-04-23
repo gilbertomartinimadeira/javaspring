13f9c6fecb258cede1aab57d896814f19dd80284
Gilberto Martini <gilberto.madeira@outlook.com>
2025-04-23 14:46:05 -0300

    fixes school controller

--- example/example/src/main/java/br/com/crossmade/example/SchoolController.java
+++ example/example/src/main/java/br/com/crossmade/example/SchoolController.java
@@ -1,5 +1,23 @@
 package br.com.crossmade.example;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
 public class SchoolController {
+
+    private final SchoolRepository schoolRepository;
+
+    public SchoolController(SchoolRepository schoolRepository) {
+        this.schoolRepository = schoolRepository;
+    }
+
+    @PostMapping("/schools")
+    public String postMethodName(@RequestBody String entity) {
+        // TODO: process POST request
+
+        return entity;
+    }
 
 }
